4db7839ab43aeb20c304a9ccb56f225869972246
Amemori <vokey5258fn@gmail.com>
2026-06-02 18:44:19 +0900

    feat: visionセクションとc-button-moreコンポーネントの作成
    - p-visionセクションのマークアップとレイアウトを実装
    - inline-gridを使用した柔軟なc-button-moreコンポーネントを作成

--- wordpress/themes/tetote/template-parts/front-page/section-vision.php
+++ wordpress/themes/tetote/template-parts/front-page/section-vision.php
@@ -1,3 +1,26 @@
 <section class="p-vision">
-    
+  <div class="l-inner p-vision__inner">
+    <p class="p-vision__text">
+      「人手不足」<br>
+      今の日本が抱えるこの社会課題に挑み、<br>
+      企業と個人の可能性を最大限に引き出す。<br>
+      それが私達の役目。
+    </p>
+
+    <p class="p-vision__text">
+      単につなぐだけじゃない。<br>
+      「手と手」を取り合っていけるような、<br>
+      持続可能な社会を、一緒に作りませんか？
+    </p>
+
+    <div class="p-vision__button-wrap">
+      <a href="<?php echo esc_url(home_url("/")); ?>" class="c-button-more">
+        <span class="c-button-more__text">View more</span>
+        <span class="u-sr-only">About usページへ</span>
+        <svg class="c-button-more__icon" aria-hidden="true" focusable="false" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M6.40298 0L3.59705 2.80555L10.7913 9.99984L3.59705 17.1945L6.40298 20L16.403 9.99984L6.40298 0Z" fill="#222222" />
+        </svg>
+      </a>
+    </div>
+  </div>
 </section>
