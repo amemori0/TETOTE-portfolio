3f3fc985c61bf778953ddf21e2dabcb4b4aab9f4
Amemori <vokey5258fn@gmail.com>
2026-06-07 16:55:13 +0900

    feat: CTAセクションの作成

--- wordpress/themes/tetote/template-parts/cta.php
+++ wordpress/themes/tetote/template-parts/cta.php
@@ -1,3 +1,20 @@
 <section class="p-cta">
-    
+  <div class="p-cta__inner">
+    <div class="p-cta__content">
+      <p class="p-cta__text">
+        わたしたちと一緒に働く仲間を募集中です。<br>
+        少数精鋭のチームで、<br>
+        あなたも会社も一緒に成長していきましょう。
+      </p>
+
+      <div class="p-cta__button-wrap">
+        <a href="<?php echo esc_url(home_url("")); ?>" class="c-button c-button--arrow c-button--primary">
+          <span class="c-button__link">Entry</span>
+          <svg class="c-button__icon" aria-hidden="true" focusable="false" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M5.12237 0L2.87762 2.24444L8.63306 7.99988L2.87762 13.7556L5.12237 16L13.1224 7.99988L5.12237 0Z" fill="#222222"/>
+          </svg>
+        </a>
+      </div>
+    </div>
+  </div>
 </section>
