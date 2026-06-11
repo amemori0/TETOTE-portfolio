2fa448db2a015d36b549d9af70ab91528761c6bf
Amemori <vokey5258fn@gmail.com>
2026-06-11 14:46:52 +0900

    feat:スタッフ一覧の個人ページのカスタムフィールドの部分の作成

--- wordpress/themes/tetote/single-staff.php
+++ wordpress/themes/tetote/single-staff.php
@@ -1,7 +1,8 @@
 <?php get_header(); ?>
 
 <main class="l-main">
-
+    <?php get_template_part("template-parts/single/staff-detail"); ?>
+    <?php get_template_part("template-parts/breadcrumb"); ?>
 </main>
 
 <?php get_footer(); ?>
---
+++ wordpress/themes/tetote/template-parts/single/staff-detail.php
@@ -0,0 +1,30 @@
+<article class="p-staff-detail">
+  <div class="p-staff-detail__inner l-inner">
+    <div class="p-staff-detail__content">
+      <h2 class="p-staff-detail__catch">
+        <span class="p-staff-detail__catch-box"><?php the_field("staff_message_01"); ?></span>
+        <span class="p-staff-detail__catch-box"><?php the_field("staff_message_02"); ?></span>
+      </h2>
+
+      <div class="p-staff-detail__meta">
+        <p class="p-staff-detail__occupation"><?php the_field("staff_category"); ?></p>
+        <div class="p-staff-detail__name-wrap">
+          <h1 class="p-staff-detail__name"><?php the_title(); ?></h1>
+          <span class="p-staff-detail__year"><?php the_field("staff_joining_year"); ?></span>
+        </div>
+      </div>
+      
+      <div class="p-staff-detail__profile">
+        <p class="p-staff-detail__text"><?php the_field("staff_profile_summary"); ?></p>
+      </div>
+    </div>
+
+    <div class="p-staff-detail__wrap">
+      <?php if (has_post_thumbnail()): ?>
+        <?php the_post_thumbnail("full", ["class" => "p-staff-detail__img"]); ?>
+      <?php else: ?>
+        <img class="p-staff-detail__img" src="<?php echo esc_url(get_theme_file_uri("/images/assets/image_dummy.webp")); ?>" alt="" />
+      <?php endif; ?>
+    </div>
+  </div>
+</article>
