6e0775575b1aa9b2215a3f257c2fd3a325606e4f
Amemori <vokey5258fn@gmail.com>
2026-06-11 17:52:45 +0900

    feat(staff): スタッフ詳細本文の余白システムとリストスタイルを調整
    - :where() と隣接セレクタによる詳細度0の堅牢なマージンシステムを導入

--- wordpress/themes/tetote/single-staff.php
+++ wordpress/themes/tetote/single-staff.php
@@ -3,6 +3,7 @@
 <main class="l-main">
     <?php get_template_part("template-parts/single/staff-detail"); ?>
     <?php get_template_part("template-parts/breadcrumb"); ?>
+    <?php get_template_part("template-parts/single/staff-detail-content"); ?>
 </main>
 
 <?php get_footer(); ?>
---
+++ wordpress/themes/tetote/template-parts/single/staff-detail-content.php
@@ -0,0 +1,8 @@
+<div class="p-staff-detail-content">
+  <div class="l-inner">
+    <div class="p-staff-detail-content__main">
+      <?php the_content(); ?>
+    </div>
+    <?php get_template_part("template-parts/single/staff-sidebar-toc"); ?>
+  </div>
+</div>
